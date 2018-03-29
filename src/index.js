09bc902b28c8d659bf0ffd6633f1064477923d80
chinhqn <chinhsoftoneqn@gmail.com>
2018-03-29 23:06:32 +0700

    add boostrap react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
- import App from './App';
-import App from './Demo;
+import App from './App';
+// import Demo from './Demo';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Demo document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
